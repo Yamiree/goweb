d32713996eb622f54fa48b02a59fc509a8c3322b
Yamiree <febriyfig@gmail.com>
2022-12-08 16:12:56 +0700

    hello world

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handlerIndex(w http.ResponseWriter, r *http.Request) {
+	var message = "Welcome guys"
+	w.Write([]byte(message)) //method
+} //*fucn almost same *method on java but func itu yang merangkul atau mewadahi method
+
+func handlerHello(w http.ResponseWriter, r *http.Request) {
+	var message = "Hello world!"
+	w.Write([]byte(message)) //parse nilai balik data byte ke
+}
+
+func main() {
+	http.HandleFunc("/", handlerIndex)
+	http.HandleFunc("/index", handlerIndex)
+	http.HandleFunc("/hello", handlerHello)
+
+	var address = "localhost:80"
+	fmt.Printf("server started at %s\n", address)
+	err := http.ListenAndServe(address, nil)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
